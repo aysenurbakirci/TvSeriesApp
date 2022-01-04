3a8a96281bbad94746637381d03b66db5259f5c9
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-01-04 13:51:36 +0300

    Create project

---
+++ TVSeriesApp/TVSeriesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVSeriesApp
+//
+//  Created by Ayşenur Bakırcı on 4.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
