61047ac58662d0925e82dbd356ae6f20d6a50849
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-01-04 14:03:22 +0300

    Add workspace and API framework

---
+++ TVSeriesAPI/TVSeriesAPI/TVSeriesAPI/TVSeriesAPI.h
@@ -0,0 +1,18 @@
+//
+//  TVSeriesAPI.h
+//  TVSeriesAPI
+//
+//  Created by Ayşenur Bakırcı on 4.01.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TVSeriesAPI.
+FOUNDATION_EXPORT double TVSeriesAPIVersionNumber;
+
+//! Project version string for TVSeriesAPI.
+FOUNDATION_EXPORT const unsigned char TVSeriesAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TVSeriesAPI/PublicHeader.h>
+
+
